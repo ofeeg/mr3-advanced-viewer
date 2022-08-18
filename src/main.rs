49612764050c3dfaa43dfd4ac3f30607d52b713b
Mason Ray Hanna III <mhanna0000@gmail.com>
2022-08-18 10:36:52 -0400

    Added multi threading so as to update the data while retaining the ability to interact with the UI.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-use iced::{button,  Container, container, Alignment, Button, Column, Element, Sandbox, Settings, Text, Row, Color, Background,};
-use mr3_data::MonMove;
-mod mr3_data;
-pub fn main() -> iced::Result {
+use iced::{Application, Background, Color, container, Element,  Settings, Command, executor, Text,   futures::executor::block_on,  Subscription, Container, Alignment, Column, Row, button, Button, Length, widget::scrollable};
+mod view_data;
+use lazy_static::__Deref;
+use view_data::{ViewData, connect_process, MR3_D, M_L_S, M_L_R, M_P_R, M_P_S, M_I_S, M_I_R, M_S_S, M_S_R, M_D_S, M_D_R, LIFESPAN, INITSPAN, FATIGUE, SPOIL, FORM, PROTEIN, VITAMIN, MINERAL, M_G_R, STRESS, FEAR, MonMove};
+
+pub fn main() -> iced::Result
+{
     Viewer::run(Settings::default())
 }
-
 
 struct StatRate{
     rate: String,
@@ -31,45 +33,6 @@ impl container::StyleSheet for StatRate
     }
 }
 
-type ViewData = (
-    (u16, u16, u16, u16, u16,
-     u16, u16,
-     u8, u8, u8, u8,
-     u8, u8, u8, u8,
-     u8
-    ),
-	u32,
-	(
-	    u8, u8, u8, u8, u8
-	),
-	(MonMove,
-	MonMove,
-	MonMove,
-	 MonMove),
-    [String;4]
-);
-lazy_static::lazy_static!{static ref MR3_D: std::sync::Mutex<ViewData> = std::sync::Mutex::new(((0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,69),
-		  0,
-		  (69, 69, 69, 69, 69),
-			      ((0,0,0,0,0,0,0,0,0,0,0,0), (0,0,0,0,0,0,0,0,0,0,0,0), (0,0,0,0,0,0,0,0,0,0,0,0), (0,0,0,0,0,0,0,0,0,0,0,0)), ["QWERTYUIOPASDFGH".to_string(),"QWERTYUIOPASDFGH".to_string(),"QWERTYUIOPASDFGH".to_string(),"QWERTYUIOPASDFGH".to_string()]),
-) ;}
-
-struct Viewer {
-    update_button: button::State,
-    move_button: button::State,
-    data: Rc<&'static MR3_D>,
-}
-
-struct ViewerSubM {
-    data: Rc<&'static MR3_D>
-}
-
-#[derive(Debug, Clone)]
-enum Message {
-    UpdatePressed,
-    MovePressed,
-}
-
 
 macro_rules! sr {
     ($s:expr) => {
@@ -93,199 +56,157 @@ macro_rules! sr {
 macro_rules! val {
     ($data:expr, $desc:literal) => {
 	 Column::new()
-	    .padding(20)
+	    .padding(10)
 	    .align_items(Alignment::Center)
 	    .push(Text::new($data))
 	    .push(Text::new($desc))
     };
-    /*($data:expr, $desc:literal, $highlight:literal) => {
-	Column::new()
-	    .padding(20)
-	    .align_items(Alignment::Center)
-	    .push(Text::new($data))
-	    .push(Text::new($desc))
-	    .push(Text::new($highlight.to_string()))
-    };*/
 }
-macro_rules! monget
+
+struct Viewer {
+    data : ViewData,
+    move_button: button::State,
+    shrink_button: button::State,
+    show_moves: bool,
+    scrollbar: scrollable::State
+}
+
+#[derive(Debug, Clone)]
+enum Message{
+    Update(view_data::Progress),
+    MovesPressed,
+    ShrinkMPressed,
+}
+
+impl Application for Viewer
 {
-    ($self:ident, $loc:tt, $loc2:tt) => {
-	match $self.data.lock()
+    type Message = Message;
+    type Executor = executor::Default;
+    type Flags = ();
+    
+    fn new(_flags: ()) -> (Viewer, Command<Message>)
+    {
+	(
+	    Viewer{
+		data:  MR3_D.try_lock().expect("Ask for data").deref().clone(),
+		move_button: button::State::new(),
+		shrink_button: button::State::new(),
+		show_moves: false,
+		scrollbar: scrollable::State::new()
+		/*scrollbar: scrollable::Scrollable::new(&mut self.scrollbar)
+		    .width(Length::Fill)
+		    .height(Length::Units(100))
+		    .style(style::Theme)
+		    .push(Space::with_height(Length::Units(800)))
+		  */  
+	    
+	    },
+	    Command::none()
+	)
+    }
+    fn title(&self) -> String{String::from("multi-threading")}
+
+    fn subscription(&self) -> Subscription<Message> {
+	connect_process().map(Message::Update)
+    }
+    fn update(&mut self, message: Message) -> Command<Message>
+    {
+	match message
 	{
-	    Ok(y) => {y.$loc.$loc2},
-	    Err(err) => panic!("{}", err),
-	}
-    };
-}
-
-
-
-use std::rc::Rc;
-impl Sandbox for Viewer {
-    type Message = Message; 
-    fn new() -> Self {
-	Viewer{
-	    update_button: button::State::new(),
-	    move_button: button::State::new(),
-	    data: Rc::new(&MR3_D),
-	}
-	
-    }
-    fn title(&self) -> String {
-        String::from("MR3 Advanced Viewer")
-    }
-
-    fn update(&mut self, message: Message)  {
-        match message {
-            Message::UpdatePressed => {
-		*self.data.lock().unwrap() = match mr3_data::connect_to_mr3()
-		{
-		    Ok(it) =>  it,
-		    Err(err) => panic!("{}", err),
+	    Message::Update(progress) => {
+		match progress {
+		    view_data::Progress::Started(_s) =>{},
+		    view_data::Progress::Finished => {block_on(async {self.data  = MR3_D.lock().await.deref().clone()});},
+		    view_data::Progress::Errored => {},
 		}
 	    },
-	    #[allow(unused_must_use)]
-	    Message::MovePressed => {ViewerSubM::run(Settings::default());}
+	    Message::MovesPressed => {self.show_moves = true;}
+	    Message::ShrinkMPressed => {self.show_moves = false;}
 	}
+	Command::none()
     }
 
     fn view(&mut self) -> Element<Message> {
-        let button_column = Column::new()
-            .padding(20)
-            .align_items(Alignment::Center)
-            .push(
-		Button::new(&mut self.update_button, Text::new("Update"))
-		    .on_press(Message::UpdatePressed),
-	    ).into();
-	let button_column2 = Column::new()
-            .padding(20)
-            .align_items(Alignment::Center)
-            .push(
-		Button::new(&mut self.move_button, Text::new("Movelist"))
-		    .on_press(Message::MovePressed),
-	    ).into();
+	let move_button = Column::new()
+	    .padding(20)
+	    .align_items(Alignment::Center)
+	    .push(Button::new(&mut self.move_button, Text::new("Moves"))
+		  .on_press(Message::MovesPressed),
+		  ).into();
+	let mon_lif = Container::new(val!(((self.data.stat_data.battle_stats[M_L_S])/10u16).to_string()
+			    + sr!(self.data.stat_data.stat_rates[M_L_R]), "LIF"))
+	    .style(StatRate{rate: (sr!(self.data.stat_data.stat_rates[5]).to_string())}).into();
+	let mon_pow = Container::new(val!(((self.data.stat_data.battle_stats[M_P_S])/10u16).to_string()
+			    + sr!(self.data.stat_data.stat_rates[M_P_R]), "POW"))
+	    .style(StatRate{rate: (sr!(self.data.stat_data.stat_rates[M_P_R]).to_string())}).into();
+	let mon_int = Container::new(val!(((self.data.stat_data.battle_stats[M_I_S])/10u16).to_string()
+			    + sr!(self.data.stat_data.stat_rates[M_I_R]), "INT"))
+	    .style(StatRate{rate: (sr!(self.data.stat_data.stat_rates[M_I_R]).to_string())}).into();
+	let mon_spd = Container::new(val!(((self.data.stat_data.battle_stats[M_S_S])/10u16).to_string()
+			    + sr!(self.data.stat_data.stat_rates[M_S_R]), "SPD"))
+	    .style(StatRate{rate: (sr!(self.data.stat_data.stat_rates[M_S_R]).to_string())}).into();
+	let mon_def = Container::new(val!(((self.data.stat_data.battle_stats[M_D_S])/10u16).to_string()
+			    + sr!(self.data.stat_data.stat_rates[M_D_R]), "DEF"))
+	    .style(StatRate{rate: (sr!(self.data.stat_data.stat_rates[M_D_R]).to_string())}).into();
+	let mon_lifespan = val!(((self.data.stat_data.battle_stats[LIFESPAN])/10u16).to_string(), "Lifespan").into();
+	let mon_initspan = val!(((self.data.stat_data.battle_stats[INITSPAN])/10u16).to_string(), "Initial Lifespan").into();
+	let mon_gr = val!(((self.data.stat_data.invisible_stats[M_G_R])/10u8).to_string(), "Growth Rate").into();
+	let mon_fatigue =  val!(((self.data.stat_data.invisible_stats[FATIGUE])/10u8).to_string(), "Fatigue").into();
+	let mon_stress = val!(((self.data.stat_data.invisible_stats[STRESS])/10u8).to_string(), "Stress").into();
+	let mon_spoil =  val!(((self.data.stat_data.invisible_stats[SPOIL])/10u8).to_string(), "Spoil").into();
+	let mon_fear = val!(((self.data.stat_data.invisible_stats[FEAR])/10u8).to_string(), "Fear").into();
+	let mon_form =  val!(((self.data.stat_data.invisible_stats[FORM])/10u8).to_string(), "Form").into();
+	let mon_protein =  val!(((self.data.stat_data.invisible_stats[PROTEIN])/10u8).to_string(), "Protein").into();
+	let mon_vitamin =  val!(((self.data.stat_data.invisible_stats[VITAMIN])/10u8).to_string(), "Vitamin").into();
+	let mon_mineral = val!(((self.data.stat_data.invisible_stats[MINERAL])/10u8).to_string(), "Mineral").into();
+	
+	let special_column =
+	    if self.show_moves == true {
+		Row::new().push(Button::new(&mut self.shrink_button, Text::new("Shrink"))
+				.on_press(Message::ShrinkMPressed))
+.push(
+		Row::new().push(scrollable::Scrollable::new(&mut self.scrollbar)
+				.width(Length::Fill)
+				.height(Length::Units(400))
+				.style(ScrollableX)
+				.padding(5)
+				.push(
+				    Column::with_children(vec![
+					express_move(self.data.move_data.0, self.data.move_names[0].to_string()).into(),
+					express_move(self.data.move_data.1, self.data.move_names[1].to_string()).into(),
+					express_move(self.data.move_data.2, self.data.move_names[2].to_string()).into(),
+					express_move(self.data.move_data.3, self.data.move_names[3].to_string()).into(),
+				    ])))
+		    )
+	    }
+	else{Row::new()};
+	
+	Column::with_children(vec![
+	    Row::with_children(vec![move_button]).into(),
+	    Row::with_children(vec![mon_lif, mon_pow, mon_int, mon_spd, mon_def]).into(),
+	    Row::with_children(vec![
+		mon_lifespan, mon_initspan, mon_gr, mon_form
+	    ]).into(),
+	    Row::with_children(vec![
+		mon_protein, mon_vitamin, mon_mineral
+	    ]).into(),
+	    Row::with_children(vec![
+		mon_fatigue, mon_stress, mon_spoil, mon_fear
+	    ]).into(),
+	    special_column.into()
+	    // Row::new()
+	    // 	.spacing(20)
+	    // 	.height(Length::Units(100))
+	    // 	.align_items(Alignment::Center)
+	    // 	.push(special_column)
+	    // 	.into()
+	]).into()
 
-	let first_column =
-	    Container::new(
-		val!((monget!(self, 0, 0)
-				  /10u16).to_string() + sr!(monget!(self,2,0)), "LIF"))
-	    .style(
-		StatRate{rate: sr!(monget!(self,2,0)).to_string()}).into();
-	
-	let second_column =
-	    Container::new(val!(((monget!(self,0,1))/10u16).to_string() +
-				sr!(monget!(self,2,1)), "POW")).style(StatRate{rate: sr!(monget!(self,2,1)).to_string()}).into();
-	let third_column = 
-	    Container::new(val!(((monget!(self,0,2))/10u16).to_string() +
-			    sr!(monget!(self,2,2)),"INT")).style(StatRate{rate: sr!(monget!(self,2,2)).to_string()}).into();
-	let fourth_column = Container::new(val!(((monget!(self,0,3))/10u16).to_string() +
-						sr!(monget!(self,2,3)),"SPD")).style(StatRate{rate: sr!(monget!(self,2,3)).to_string()}).into();
-	let fifth_column = Container::new(val!(((monget!(self,0,4))/10u16).to_string() +
-			    sr!(monget!(self,2,4)),"DEF")).style(StatRate{rate: sr!(monget!(self,2,4)).to_string()}).into();
-	
-	let sixth_column = val!(((match self.data.lock()
-				  {
-				      Ok(y) => y.0.5,
-				      Err(err) => panic!("{}", err),
-				  }
-	)/10u16).to_string(), "Lifespan").into();
-
-	let seventh_column = 
-	    val!(((match self.data.lock()
-		   {
-		       Ok(y) => y.0.6,
-		       Err(err) => panic!("{}", err),
-		   }
-	    )/10u16).to_string(), "InitialSpan").into();
-	let eigth_column = 
-	    val!(((match self.data.lock()
-		   {
-		       Ok(y) => y.0.7,
-		       Err(err) => panic!("{}", err),
-		   }
-	    )).to_string(), "Fatigue").into();
-	let ninth_column =  
-	    val!(((match self.data.lock()
-		   {
-		       Ok(y) => y.0.8,
-		       Err(err) => panic!("{}", err),
-		   }
-	    )).to_string(), "Stress").into();
-	let tenth_column =  
-	    val!(((match self.data.lock()
-		   {
-		       Ok(y) => y.0.9,
-		       Err(err) => panic!("{}", err),
-		   }
-	    )).to_string(), "Fear").into();
-	let eleventh_column =
-	    val!(((match self.data.lock()
-		   {
-		       Ok(y) => y.0.10,
-		       Err(err) => panic!("{}", err),
-		   }
-	    )).to_string(), "Spoil").into();
-	
-	let twelfth_column = 
-	    val!(((match self.data.lock()
-		   {
-		       Ok(y) => y.0.11,
-		       Err(err) => panic!("{}", err),
-		   }
-	    )).to_string(), "Form").into();
-	let thirteenth_column = 
-	    val!(((match self.data.lock()
-		   {
-		       Ok(y) => y.0.12,
-		       Err(err) => panic!("{}", err),
-		   }
-	    )).to_string(), "Protein").into();
-	let fourteenth_column = 
-	    val!(((match self.data.lock()
-		   {
-		       Ok(y) => y.0.13,
-		       Err(err) => panic!("{}", err),
-		   }
-	    )).to_string(), "Vitamin").into();
-	let fifteenth_column = 
-	    val!(((match self.data.lock()
-		   {
-		       Ok(y) => y.0.14,
-		       Err(err) => panic!("{}", err),
-		   }
-	    )).to_string(), "Mineral").into();
-	let seventeenth_column = 
-	    val!(mr3_data::determine_gr(match self.data.lock()
-					{
-					    Ok(y) => y.0.15,
-					    Err(err) => panic!("{}", err),
-					}
-	    )
-		 , "GrowthRate").into();
-	let sixteenth_column =
-	    val!((match self.data.lock()
-		  {
-		      Ok(y) => y.1,
-		      Err(err) => panic!("{}", err),
-		  }).to_string(),"Money").into();
-	
-	Column::with_children(vec!
-			      [
-				  button_column,
-				  Row::with_children(vec![first_column, second_column, third_column, fourth_column,fifth_column]).into(),
-				  Row::with_children(vec![sixth_column, seventh_column, seventeenth_column]).into(),
-				  Row::with_children(vec![eigth_column, ninth_column,tenth_column, eleventh_column]).into(),
-				  Row::with_children(vec![twelfth_column,thirteenth_column, fourteenth_column, fifteenth_column, sixteenth_column]).into(),
-				  button_column2,
-			      ]
-			      ).into()
     }
-
-}				      
     
-    
+}
 
-fn express_move<'a>(m: mr3_data::MonMove, c: String) -> Row<'a, Message>
+fn express_move<'a>(m: MonMove, c: String) -> Row<'a, Message>
 {
     let mut _s: String = "".to_string();
     _s.push_str(&c.as_str());
@@ -306,76 +227,77 @@ fn express_move<'a>(m: mr3_data::MonMove, c: String) -> Row<'a, Message>
     //let mov_elem = val!(m.13.to_string(), "Element");
     Row::with_children(vec!
 		       [
-			   mov_name, mov_lvl, mov_maxlvl,
-			   mov_type, mov_xp, mov_cost,
-			   mov_acc, mov_crit, mov_dmg,
-			   mov_wit, mov_eff1, //mov_eff2,
-			   //mov_elem
-		       ])
+			   mov_name, mov_type, mov_cost,
+			   Row::with_children(vec![
+			       mov_lvl, mov_maxlvl, mov_xp,
+			   ]).into(),
+			   Row::with_children(vec![
+			       mov_dmg, mov_acc, mov_crit, mov_wit
+			   ]).into(),
+			   Row::with_children(vec![
+			       mov_eff1
+			   ]).into()
+			   
+		       ]).padding(5)
 }
 
+pub struct ScrollableX;
 
-impl Sandbox for ViewerSubM {
-    type Message = Message;
-    fn new() -> Self
-    {
-	ViewerSubM{
-	    data: Rc::new(&MR3_D),
-	}
+impl scrollable::StyleSheet for ScrollableX {
+    fn active(&self) -> scrollable::Scrollbar {
+        scrollable::Scrollbar {
+            background: SURFACE.into(),
+            border_radius: 2.0,
+            border_width: 0.0,
+            border_color: Color::TRANSPARENT,
+            scroller: scrollable::Scroller {
+                color: ACTIVE,
+                border_radius: 2.0,
+                border_width: 0.0,
+                border_color: Color::TRANSPARENT,
+            },
+        }
     }
-    fn title(&self) -> String {
-	String::from("Movelist")
-    }
-    fn update(&mut self, _message: Self::Message) {
+    
+    fn hovered(&self) -> scrollable::Scrollbar {
+        let active = self.active();
 	
+        scrollable::Scrollbar {
+            background: Color { a: 0.5, ..SURFACE }.into(),
+            scroller: scrollable::Scroller {
+                color: HOVERED,
+                ..active.scroller
+            },
+            ..active
+        }
     }
-    fn view(&mut self) -> Element<Message>
-    {
-	let _1 = match self.data.lock()
-					       {
-						   Ok(y) => {y.4[0].clone()},
-						   Err(err) => panic!("{}", err)
-					       };
-	let _2 = match self.data.lock()
-					       {
-						   Ok(y) => {y.4[1].clone()},
-						   Err(err) => panic!("{}", err)
-					       };
-
-	let _3 = match self.data.lock()
-					       {
-						   Ok(y) => {y.4[2].clone()},
-						   Err(err) => panic!("{}", err)
-					       };
-
-	let _4 = match self.data.lock()
-					       {
-						   Ok(y) => {y.4[3].clone()},
-						   Err(err) => panic!("{}", err)
-					       };
+    
+    fn dragging(&self) -> scrollable::Scrollbar {
+        let hovered = self.hovered();
 	
-	Column::with_children(vec![
-	    express_move(match self.data.lock()
-				  {
-				      Ok(y) => y.3.0,
-				      Err(err) => panic!("{}", err),
-				  },_1).into(),
-	    express_move(match self.data.lock()
-				  {
-				      Ok(y) => y.3.1,
-				      Err(err) => panic!("{}", err),
-				  },_2).into(),
-	    express_move(match self.data.lock()
-				  {
-				      Ok(y) => y.3.2,
-				      Err(err) => panic!("{}", err),
-				  },_3).into(),
-	    express_move(match self.data.lock()
-				  {
-				      Ok(y) => y.3.3,
-				      Err(err) => panic!("{}",err),
-				  },_4).into()
-	]).into()
+        scrollable::Scrollbar {
+            scroller: scrollable::Scroller {
+                color: Color::from_rgb(0.85, 0.85, 0.85),
+                ..hovered.scroller
+            },
+            ..hovered
+        }
     }
 }
+const SURFACE: Color = Color::from_rgb(
+    0x40 as f32 / 255.0,
+    0x44 as f32 / 255.0,
+    0x4B as f32 / 255.0,
+);
 
+const ACTIVE: Color = Color::from_rgb(
+    0x72 as f32 / 255.0,
+    0x89 as f32 / 255.0,
+    0xDA as f32 / 255.0,
+);
+
+const HOVERED: Color = Color::from_rgb(
+    0x67 as f32 / 255.0,
+    0x7B as f32 / 255.0,
+    0xC4 as f32 / 255.0,
+);
